6defa73885db5b605897f3f699b011f249b3d2a3
Mounjee <ericy@LAPTOP-MO4RYS8.localdomain>
2022-11-04 18:01:12 -0400

    Created Example action

--- task_selector/task_selector/action_server_node.py
+++ task_selector/task_selector/action_server_node.py
@@ -6,3 +6,14 @@ class TaskController(Node):
     
     def __init__(self):
         super().__init__('task_controller')
+        self._action_server = ActionServer(
+            self,
+            Example,
+            'cool example',
+            self.execute_callback
+        )
+        
+    def execute_callback(self, goal_handle):
+        self.get_logger().info('Executing goal...')
+        result = Example.Result()
+        return result
