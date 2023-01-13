963d1ccf8b5ef4ec472a4d85715c6b8f3ffb9f6a
rickyrnt <rick.yarnot.255@gmail.com>
2023-01-12 16:53:33 -0800

    Added comments for clarity

--- surface/task_selector/task_selector/task_requestor.py
+++ surface/task_selector/task_selector/task_requestor.py
@@ -12,24 +12,17 @@ class TaskRequestor(Node):
     def __init__(self):
         # creation of a Node with its name as input
         super().__init__('task_requestor')
-        # instantiates new action client with inputs of node, action type, action name
+        
+        # create service to handle requests for task switching
+        
+        # instantiates new action clients with inputs of node, action type, action name
         self.morning_action_client = ActionClient(self, Example, 'say_good_morning')
-        self.basic_task_client = ActionClient(self, BasicTask, 'timed_task')
+        self.timed_task_client = ActionClient(self, BasicTask, 'timed_task')
+        self.basic_task_client = ActionClient(self, BasicTask, 'basic_task')
         
-    
-    
-    def send_morning_goal(self, morning, cheery):
-        goal_msg = Example.Goal()
-        goal_msg.morning = morning
-        goal_msg.cheery = cheery
         
-        self.get_logger().info('Waiting for action server...')
-        self.morning_action_client.wait_for_server()
-        
-        self.get_logger().info('Sending goal request...')
-        self._send_goal_future = self.morning_action_client.send_goal_async(goal_msg, feedback_callback = self.feedback_callback)
-        self._send_goal_future.add_done_callback(self.goal_response_callback)
-        
+    # Basic task client takes no input variables and only recieves feedback, no result data
+    # send_basic_goal() takes a basic client and requests for the server it's attached to to run a task
     def send_basic_goal(self, client):
         goal_msg = BasicTask.Goal()
         
@@ -40,18 +33,21 @@ class TaskRequestor(Node):
         self._send_goal_future = client.send_goal_async(goal_msg, feedback_callback = self.feedback_callback)
         self._send_goal_future.add_done_callback(self.basic_response_callback)
         
+    # A Say Good Morning server takes the time of day and cheeriness to produce a greeting
+    def send_morning_goal(self, morning, cheery):
+        goal_msg = Example.Goal()
+        goal_msg.morning = morning
+        goal_msg.cheery = cheery
+        
+        self.get_logger().info('Waiting for action server...')
+        self.morning_action_client.wait_for_server()
+        
+        self.get_logger().info('Sending goal request...')
+        self._send_goal_future = self.morning_action_client.send_goal_async(goal_msg, feedback_callback = self.feedback_callback)
+        self._send_goal_future.add_done_callback(self.morning_response_callback)
+        
+        
     # Checks if goal was accepted
-    def goal_response_callback(self, future):
-        goal_handle = future.result()
-        if not goal_handle.accepted:
-            self.get_logger().info('Goal rejected')
-            return
-
-        self.get_logger().info('Goal accepted')
-        
-        self._get_result_future = goal_handle.get_result_async()
-        self._get_result_future.add_done_callback(self.get_result_callback)
-        
     def basic_response_callback(self, future):
         goal_handle = future.result()
         if not goal_handle.accepted:
@@ -64,22 +60,34 @@ class TaskRequestor(Node):
         
         self._get_result_future = goal_handle.get_result_async()
         self._get_result_future.add_done_callback(self.basic_result_callback)
+        
+    def morning_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected')
+            return
 
+        self.get_logger().info('Goal accepted')
+        
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.morning_result_callback)
+        
+
+    # Notify us that task is finished
     def basic_result_callback(self, future):
-        #result = future.result().result
         self.get_logger().info("Task finished")
-        #rclpy.shutdown()
 
-    # Logs result, exits
-    def get_result_callback(self, future):
+    # Logs greeting that the morning server sends
+    def morning_result_callback(self, future):
         result = future.result().result
         self.get_logger().info('Result: {0}'.format(result.message))
-        #rclpy.shutdown()
+        
 
     # Logs feedback from action server
     def feedback_callback(self, feedback_msg):
         feedback = feedback_msg.feedback
         self.get_logger().info('Received feedback: {0}'.format(feedback.feedback_message))
+        
         
     # Only works if server runs on a multithreaded executor
     def cancel_goal(self):
@@ -95,10 +103,6 @@ class TaskRequestor(Node):
             self.get_logger().info('Goal successfully canceled')
         else:
             self.get_logger().info('Goal failed to cancel')
-        #rclpy.shutdown()
-        
-    def timer_callback(self):
-        self.cancel_goal()
 
 
     
@@ -107,7 +111,7 @@ def main(args=None):
     
     action_client = TaskRequestor()
     
-    action_client.send_basic_goal(action_client.basic_task_client)
+    action_client.send_basic_goal(action_client.timed_task_client)
     cancelled = False
     timer = 0
     while True:
