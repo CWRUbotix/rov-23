90c9acd3b2ce469795a7338a79d6ffd35bfa0ee8
NoahMollerstuen <noah@mollerstuen.com>
2023-01-26 18:15:21 -0500

    Include launch files

--- pi/camera_streamer/setup.py
+++ pi/camera_streamer/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from glob import glob
+import os
 
 package_name = 'camera_streamer'
 
@@ -10,6 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include all launch files.
+        (os.path.join('share', package_name, 'launch'),
+         glob('launch/*launch.[pxy][yma]*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
