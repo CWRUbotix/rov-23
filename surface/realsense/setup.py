eea74eeab28efa255923c64cd15959524a6ae308
Tazmania40 <tzupfer@gmail.com>
2023-01-27 20:38:03 -0500

    Making a package for future realsense endevors

---
+++ surface/realsense/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+
+package_name = 'realsense'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+                # Include all launch files.
+        (os.path.join('share', package_name, 'launch'),
+         glob('launch/*launch.[pxy][yma]*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tzupfer',
+    maintainer_email='tzupfer@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
