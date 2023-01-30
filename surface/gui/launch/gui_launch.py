80d243e8ba6197cc91ba4ca36a644acdc0317b8a
Michael Carlstrom <rmc170@case.edu>
2023-01-30 14:34:17 -0800

    remove docstring newline

--- surface/gui/launch/gui_launch.py
+++ surface/gui/launch/gui_launch.py
@@ -5,7 +5,6 @@ from multiprocessing import Process
 
 def generate_launch_description():
     """Asynchronously ROS launch the GUI."""
-
     p = Process(target=run_gui, args=())
     p.start()
     return launch.LaunchDescription([])
