70b7d3510382ecbe6de1c8e2d7d0d68c744c7338
Michael Carlstrom <rmc170@case.edu>
2023-01-30 14:39:16 -0800

    nvm ben I lied

--- surface/gui/launch/gui_launch.py
+++ surface/gui/launch/gui_launch.py
@@ -5,5 +5,5 @@ from multiprocessing import Process
 
 def generate_launch_description():
     """Asynchronously ROS launch the GUI."""
-    Process(target=run_gui, args=()).start()
+    Process(target=run_gui).start()
     return launch.LaunchDescription([])
