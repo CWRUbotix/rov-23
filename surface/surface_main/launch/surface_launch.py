2e17e34da8175136a53317a5d83e34cf11d89ee0
Michael Carlstrom <rmc170@case.edu>
2023-02-03 17:56:58 -0800

    fixed launch file

--- surface/surface_main/launch/surface_launch.py
+++ surface/surface_main/launch/surface_launch.py
@@ -9,7 +9,7 @@ def generate_launch_description():
 
     gui_path: str = get_package_share_directory('gui')
     task_selector_path: str = get_package_share_directory('task_selector')
-    controller_path: str = get_package_share_directory('ps5-controller')
+    controller_path: str = get_package_share_directory('ps5_controller')
     # Launches Gui
     gui_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
