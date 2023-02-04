8e38d2eefabe56ee42df02968da727b3418e0260
Michael Carlstrom <rmc170@case.edu>
2023-02-04 12:08:01 -0800

    test2

--- surface/task_selector/test/test_manual_control_node.py
+++ surface/task_selector/test/test_manual_control_node.py
@@ -3,8 +3,6 @@ from task_selector.manual_control_node import ControlNode
 
 def manual_control_buttons():
     node = ControlNode()
-    node.passing = True
-
     # When unpressed should do nothing
     assert 0 == node.l2_r2_math(1, 1)
     # When r2 pressed should spin right
