33bda7d56555172373d3df5a6aa6fc3b2132d76c
Michael Carlstrom <rmc170@case.edu>
2023-02-16 20:47:31 -0800

    fixed setup.cfg

--- pi/pixhawk_communication/pixhawk_communication/pixhawk_communication.py
+++ pi/pixhawk_communication/pixhawk_communication/pixhawk_communication.py
@@ -9,7 +9,7 @@ from rov_interfaces.msg import Armed, ROVControl
 class PixhawkCommunication(Node):
 
     def __init__(self):
-        super().__init__('Pixhawk Communication')
+        super().__init__('pixhawk_communication')
         self.arm_sub: Subscription = self.create_subscription(
             Armed,
             'armed',
@@ -21,19 +21,20 @@ class PixhawkCommunication(Node):
             self.rov_control_callback,
             100
         )
-        self.pixhawk: mavutil.mavserial = mavutil.mavlink_connection('/dev/ttyUSB0')
-        self.pixhawk.wait_heartbeat()
+        # self.pixhawk: mavutil.mavserial = mavutil.mavlink_connection('/dev/ttyUSB0')
+        # self.pixhawk.wait_heartbeat()
 
     def arm_callback(self, msg: Armed):
-        self.pixhawk.mav.command_long_send(
-            self.pixhawk.target_system,
-            self.pixhawk.target_component,
-            mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-            0,
-            # ternary for this bit for deciding arm
-            # 1 is armed 0 is disarmed
-            1 if msg.armed else 0,
-            0, 0, 0, 0, 0, 0)
+        # self.pixhawk.mav.command_long_send(
+        #     self.pixhawk.target_system,
+        #     self.pixhawk.target_component,
+        #     mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+        #     0,
+        #     # ternary for this bit for deciding arm
+        #     # 1 is armed 0 is disarmed
+        #     1 if msg.armed else 0,
+        #     0, 0, 0, 0, 0, 0)
+        print("todo")
 
     def rov_control_callback(self, msg: ROVControl):
         # TODO send manual control
