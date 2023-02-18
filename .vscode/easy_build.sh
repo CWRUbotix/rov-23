2f6800850646471ec4ba65d9a2d484c1897f2c56
Michael Carlstrom <rmc170@case.edu>
2023-02-18 08:24:46 -0800

    comments 2

--- .vscode/easy_build.sh
+++ .vscode/easy_build.sh
@@ -1,6 +1,7 @@
 # Next year could move root of clone
 # Or Make custom vscode Extension
 
+# Currently requires copying tasks.json contents into root directory .vscode/ folder
 function cdroot()
 {
   while [[ $PWD != '/' && ${PWD##*/} != 'rov_23_ws' ]]; do cd ..; done
