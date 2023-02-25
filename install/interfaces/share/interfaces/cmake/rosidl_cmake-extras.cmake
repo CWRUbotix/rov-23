7b86e16660313d6ae954753499ab61c99d500782
Georgia <gcm49@case.edu>
2023-02-25 13:09:15 -0800

    Moved manipulator toggling logic to surface instead of pi

--- install/interfaces/share/interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(interfaces_IDL_FILES "srv/TaskRequest.idl;action/Example.idl;action/BasicTask.idl;msg/TaskFeedback.idl;msg/ROVControl.idl;msg/Armed.idl;msg/Manip.idl")
-set(interfaces_INTERFACE_FILES "srv/TaskRequest.srv;srv/TaskRequest_Request.msg;srv/TaskRequest_Response.msg;action/Example.action;action/BasicTask.action;msg/TaskFeedback.msg;msg/ROVControl.msg;msg/Armed.msg;msg/Manip.msg")
