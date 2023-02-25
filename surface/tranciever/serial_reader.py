f399053edf4aa60b6838b35a973521d2a4480d8f
rickyrnt <rick.yarnot.255@gmail.com>
2023-02-24 21:00:02 -0500

    Added serial monitor read capability

--- surface/tranciever/serial_reader.py
+++ surface/tranciever/serial_reader.py
@@ -2,19 +2,26 @@ import serial
 import rclpy
 from rclpy.node import Node
 
+from std_msgs.msg import String
+
 class SerialReader(Node):
     
     def __init__(self):
         super().__init__('serial_reader')
-        self.publisher = self.create_publisher(String, 'tranciever_data', 10)
-        timer_period = 1
+        self.publisher_ = self.create_publisher(String, 'tranciever_data', 10)
+        self.listener = self.create_subscription(
+            String, 
+            'tranciever_control', 
+            self.control_callback(self), 
+            10)
+        timer_period = .5
         self.ser = serial.Serial('/dev/ttyUSB0', 115200)
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
         msg = String()
-        msg.data = self.ser.readline()
+        msg.data = self.ser.readline().decode()
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
@@ -22,7 +29,7 @@ class SerialReader(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    serial_reader = SerialReader
+    serial_reader = SerialReader()
 
     rclpy.spin(serial_reader)
 
