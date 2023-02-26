6f26b83f2c0ee6e6263eaddd1a8aded4e30c7d13
sxj754 <sxj754@case.edu>
2023-02-25 23:06:26 -0500

    Minor fix

--- surface/rov_gazebo/rov_gazebo/thruster_controller_node.py
+++ surface/rov_gazebo/rov_gazebo/thruster_controller_node.py
@@ -3,8 +3,6 @@ from rclpy.node import Node
 from std_msgs.msg import Float64
 from geometry_msgs.msg import Twist
 
-from std_msgs.msg import String
-
 topic_names = [
     "model/rov/joint/thruster_top_front_left_body_blade_joint/cmd_force",
     "model/rov/joint/thruster_top_front_right_body_blade_joint/cmd_force",
@@ -24,24 +22,21 @@ class ThrusterControllerNode(Node):
         self.subscriber_ = self.create_subscription(
             Twist, "/cmd_vel", self.callback, qos_profile=10
         )
-        self.dummy = self.create_publisher(String, "dummy", qos_profile=10)
 
     def callback(self, msg):
-        self.dummy.publish(String(data="got message"))
-        self.get_logger().info("got message")
         if msg.linear.z > 0:
-            self.publish_all(1, msg.linear.z)
+            self.z_control(1, msg.linear.z)
         elif msg.linear.z < 0:
-            self.publish_all(-1, msg.linear.z)
+            self.z_control(-1, msg.linear.z)
         else:
-            self.publish_all(0.0, 0.0)
+            self.z_control(0.0, 0.0)
 
     def create_publishers(self, msg_type, qos_profile=10):
         for topic in topic_names:
             self.publishers_.append(self.create_publisher(msg_type, topic, qos_profile))
 
-    def publish_all(self, direction, speed):
-        for publisher in self.publishers_:
+    def z_control(self, direction, speed):
+        for publisher in self.publishers_[4:7]:
             publisher.publish(Float64(data=direction * speed * 50))
 
     def spin(self):
