f1e117cd1ec261172e436f83a362b54176c66f41
sxj754 <sxj754@case.edu>
2023-02-27 02:09:56 -0500

    Enhance control logic

--- surface/rov_gazebo/rov_gazebo/thruster_controller_node.py
+++ surface/rov_gazebo/rov_gazebo/thruster_controller_node.py
@@ -22,23 +22,16 @@ class ThrusterControllerNode(Node):
     def __init__(self):
         super().__init__("thruster_controller_node")
         self.thrusters = [
-            {"location": "top_front_left", "power": 0.0},
-            {"location": "top_front_right", "power": 0.0},
-            {"location": "top_back_left", "power": 0.0},
-            {"location": "top_back_right", "power": 0.0},
-            {"location": "bottom_front_left", "power": 0.0},
-            {"location": "bottom_front_right", "power": 0.0},
-            {"location": "bottom_back_left", "power": 0.0},
-            {"location": "bottom_back_right", "power": 0.0},
+            {"location": "top_front_left"},
+            {"location": "top_front_right"},
+            {"location": "top_back_left"},
+            {"location": "top_back_right"},
+            {"location": "bottom_front_left"},
+            {"location": "bottom_front_right"},
+            {"location": "bottom_back_left"},
+            {"location": "bottom_back_right"},
         ]
-        self.xyzrpy = {
-            "x": 0.0,
-            "y": 0.0,
-            "z": 0.0,
-            "roll": 0.0,
-            "pitch": 0.0,
-            "yaw": 0.0,
-        }
+        self.multiplier = 3
 
         self.publishers_ = []
         self.subscriber_ = self.create_subscription(
@@ -55,67 +48,63 @@ class ThrusterControllerNode(Node):
             self.publishers_.append(self.create_publisher(msg_type, topic, qos_profile))
 
     def control(self, msg):
-        self.x_control(msg.linear.x)
-        self.y_control(msg.linear.y)
-        self.z_control(msg.linear.z)
-        self.roll_control(msg.angular.x)
-        self.pitch_control(msg.angular.y)
-        self.yaw_control(msg.angular.z)
+        thrust_list = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        thrust_list = self.x_control(msg.linear.x, thrust_list)
+        thrust_list = self.y_control(msg.linear.y, thrust_list)
+        thrust_list = self.z_control(msg.linear.z, thrust_list)
+        thrust_list = self.roll_control(msg.angular.x, thrust_list)
+        thrust_list = self.pitch_control(msg.angular.y, thrust_list)
+        thrust_list = self.yaw_control(msg.angular.z, thrust_list)
+        self.publish_thrust(thrust_list)
 
-    def x_control(self, speed):
-        if self.xyzrpy["x"] != speed:
-            multiplier = 3
-            prev_thruster_input = self.xyzrpy["x"] * multiplier
-            thruster_input = speed * multiplier
-            diff = thruster_input - prev_thruster_input
-            # first: add diff
-            self.thrusters[0]["power"] += diff
-            # second: subtract diff
-            self.thrusters[1]["power"] -= diff
-            # third: subtract diff
-            self.thrusters[2]["power"] -= diff
-            # fourth: add diff
-            self.thrusters[3]["power"] += diff
+    def x_control(self, speed, thrust_list):
+        thrust = speed * self.multiplier
+        # first: add thrust
+        thrust_list[0] += thrust
+        # second: subtract thrust
+        thrust_list[1] -= thrust
+        # third: subtract thrust
+        thrust_list[2] -= thrust
+        # fourth: add thrust
+        thrust_list[3] += thrust
+        return thrust_list
 
-            self.xyzrpy["x"] = speed
-            self.publish_power()
+    def y_control(self, speed, thrust_list):
+        thrust = speed * self.multiplier
+        # first: subtract thrust
+        thrust_list[0] -= thrust
+        # second: subtract thrust
+        thrust_list[1] -= thrust
+        # third: subtract thrust
+        thrust_list[2] -= thrust
+        # fourth: subtract thrust
+        thrust_list[3] -= thrust
+        return thrust_list
 
-    def y_control(self, speed):
-        if self.xyzrpy["y"] != speed:
-            multiplier = 3
-            prev_thruster_input = self.xyzrpy["y"] * multiplier
-            thruster_input = speed * multiplier
-            diff = thruster_input - prev_thruster_input
-            # first: subtract diff
-            self.thrusters[0]["power"] -= diff
-            # second: subtract diff
-            self.thrusters[1]["power"] -= diff
-            # third: subtract diff
-            self.thrusters[2]["power"] -= diff
-            # fourth: subtract diff
-            self.thrusters[3]["power"] -= diff
+    def z_control(self, speed, thrust_list):
+        thrust = speed * self.multiplier
+        # 5th: add thrust
+        thrust_list[4] += thrust
+        # 6th: add thrust
+        thrust_list[5] += thrust
+        # 7th: add thrust
+        thrust_list[6] += thrust
+        # 8th: add thrust
+        thrust_list[7] += thrust
+        return thrust_list
 
-            self.xyzrpy["y"] = speed
-            self.publish_power()
+    def roll_control(self, speed, thrust_list):
+        return thrust_list
 
-    def z_control(self, speed):
-        multiplier = 3
-        for publisher in self.publishers_[4:8]:
-            thruster_input = speed * multiplier
-            publisher.publish(Float64(data=thruster_input))
+    def pitch_control(self, speed, thrust_list):
+        return thrust_list
 
-    def roll_control(self, speed):
-        pass
+    def yaw_control(self, speed, thrust_list):
+        return thrust_list
 
-    def pitch_control(self, speed):
-        pass
-
-    def yaw_control(self, speed):
-        pass
-
-    def publish_power(self):
+    def publish_thrust(self, thrust_list):
         for i in range(len(self.thrusters)):
-            self.publishers_[i].publish(Float64(data=self.thrusters[i]["power"]))
+            self.publishers_[i].publish(Float64(data=thrust_list[i]))
 
     def spin(self):
         rclpy.spin(self)
