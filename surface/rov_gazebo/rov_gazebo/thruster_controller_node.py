0f7831d950b9322d53cf0cae5f911e1f0068c90d
sxj754 <sxj754@case.edu>
2023-02-26 23:28:27 -0500

    Fix control logic

--- surface/rov_gazebo/rov_gazebo/thruster_controller_node.py
+++ surface/rov_gazebo/rov_gazebo/thruster_controller_node.py
@@ -3,15 +3,15 @@ from rclpy.node import Node
 from std_msgs.msg import Float64
 from geometry_msgs.msg import Twist
 
-topic_names = [
-    "model/rov/joint/thruster_top_front_left_body_blade_joint/cmd_force",
-    "model/rov/joint/thruster_top_front_right_body_blade_joint/cmd_force",
-    "model/rov/joint/thruster_top_back_left_body_blade_joint/cmd_force",
-    "model/rov/joint/thruster_top_back_right_body_blade_joint/cmd_force",
-    "model/rov/joint/thruster_bottom_front_left_body_blade_joint/cmd_force",
-    "model/rov/joint/thruster_bottom_front_right_body_blade_joint/cmd_force",
-    "model/rov/joint/thruster_bottom_back_left_body_blade_joint/cmd_force",
-    "model/rov/joint/thruster_bottom_back_right_body_blade_joint/cmd_force",
+thrusters = [
+    {"location": "top_front_left", "reflect_blade": -1},
+    {"location": "top_front_right", "reflect_blade": -1},
+    {"location": "top_back_left", "reflect_blade": 1},
+    {"location": "top_back_right", "reflect_blade": 1},
+    {"location": "bottom_front_left", "reflect_blade": 1},
+    {"location": "bottom_front_right", "reflect_blade": -1},
+    {"location": "bottom_back_left", "reflect_blade": -1},
+    {"location": "bottom_back_right", "reflect_blade": 1},
 ]
 
 
@@ -25,19 +25,26 @@ class ThrusterControllerNode(Node):
 
     def callback(self, msg):
         if msg.linear.z > 0:
-            self.z_control(1, msg.linear.z)
+            self.z_control(msg.linear.z)
         elif msg.linear.z < 0:
-            self.z_control(-1, msg.linear.z)
+            self.z_control(msg.linear.z)
         else:
             self.z_control(0.0, 0.0)
 
     def create_publishers(self, msg_type, qos_profile=10):
-        for topic in topic_names:
+        for thruster in thrusters:
+            topic = (
+                "model/rov/joint/thruster_"
+                + thruster["location"]
+                + "_body_blade_joint/cmd_force"
+            )
             self.publishers_.append(self.create_publisher(msg_type, topic, qos_profile))
 
-    def z_control(self, direction, speed):
-        for publisher in self.publishers_[4:7]:
-            publisher.publish(Float64(data=direction * speed * 50))
+    def z_control(self, speed):
+        multiplier = 30
+        for i in range(4, 8):
+            thruster_input = speed * multiplier * thrusters[i]["reflect_blade"]
+            self.publishers_[i].publish(Float64(data=thruster_input))
 
     def spin(self):
         rclpy.spin(self)
