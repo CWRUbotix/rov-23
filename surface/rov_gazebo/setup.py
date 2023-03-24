d223280b68da7f1897c185c852fd8b15ee775967
Michael Carlstrom <rmc170@case.edu>
2023-03-24 16:51:18 -0700

    added robot cad

--- surface/rov_gazebo/setup.py
+++ surface/rov_gazebo/setup.py
@@ -15,6 +15,7 @@ setup(
         (os.path.join("share", package_name, "description"), glob("description/*")),
         (os.path.join("share", package_name, "config"), glob("config/*")),
         (os.path.join("share", package_name, "worlds"), glob("worlds/*")),
+        (os.path.join("share", package_name, "meshes"), glob("meshes/*")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
