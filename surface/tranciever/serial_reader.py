068bd697bbeef55938f5c63e5fbd875f6cbea8e5
Henry <hwodza@gmail.com>
2023-03-24 19:16:12 -0400

    added stuff to cobtrol callback

--- surface/tranciever/serial_reader.py
+++ surface/tranciever/serial_reader.py
@@ -12,7 +12,7 @@ class SerialReader(Node):
         self.listener = self.create_subscription(
             String, 
             'tranciever_control', 
-            self.control_callback(self), 
+            self.control_callback, 
             10)
         timer_period = .5
         self.ser = serial.Serial('/dev/ttyUSB0', 115200)
@@ -25,6 +25,10 @@ class SerialReader(Node):
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
+    
+    def control_callback(self, msg):
+        self.ser.write(msg)
+        self.get_logger().info('Command sent via serial monitor: "%s"' % msg.data)
 
 def main(args=None):
     rclpy.init(args=args)
