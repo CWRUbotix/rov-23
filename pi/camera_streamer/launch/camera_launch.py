d46a70e1d5edbc95da478824b6c80b8a594c8922
benjaminwp18 <bwp18@case.edu>
2023-04-08 13:42:15 -0400

    Test with usb_cam

--- pi/camera_streamer/launch/camera_launch.py
+++ pi/camera_streamer/launch/camera_launch.py
@@ -4,37 +4,48 @@ from launch import LaunchDescription
 
 def generate_launch_description():
 
-    # Launch the front camera node
     front_cam_node: Node = Node(
-        package="v4l2_camera",
-        executable="v4l2_camera_node",
+        package="usb_cam",
+        executable="usb_cam_node_exe",
         namespace="front_cam",
         parameters=[
-            {"video_device": "/dev/video0"}
+            {"image_width": "640"},
+            {"image_height": "480"},
+            {"pixel_format": "mjpeg2rgb"}
         ]
     )
 
     # Launch the front camera node
-    bottom_cam_node: Node = Node(
-        package="v4l2_camera",
-        executable="v4l2_camera_node",
-        namespace="bottom_cam",
-        parameters=[
-            {"video_device": "/dev/video2"}
-        ]
-    )
+    # front_cam_node: Node = Node(
+    #     package="v4l2_camera",
+    #     executable="v4l2_camera_node",
+    #     namespace="front_cam",
+    #     parameters=[
+    #         {"video_device": "/dev/video0"}
+    #     ]
+    # )
 
-    manip_cam_node: Node = Node(
-        package="v4l2_camera",
-        executable="v4l2_camera_node",
-        namespace="manip_cam",
-        parameters=[
-            {"video_device": "/dev/video6"}
-        ]
-    )
+    # Launch the front camera node
+    # bottom_cam_node: Node = Node(
+    #     package="v4l2_camera",
+    #     executable="v4l2_camera_node",
+    #     namespace="bottom_cam",
+    #     parameters=[
+    #         {"video_device": "/dev/video2"}
+    #     ]
+    # )
+
+    # manip_cam_node: Node = Node(
+    #     package="v4l2_camera",
+    #     executable="v4l2_camera_node",
+    #     namespace="manip_cam",
+    #     parameters=[
+    #         {"video_device": "/dev/video6"}
+    #     ]
+    # )
 
     return LaunchDescription([
         front_cam_node,
-        bottom_cam_node,
-        manip_cam_node
+        # bottom_cam_node,
+        # manip_cam_node
     ])
