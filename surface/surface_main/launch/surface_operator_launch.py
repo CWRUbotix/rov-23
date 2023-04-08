f34f00f84934ff98b82c0953250e729f436d9ad7
rickyrnt <rick.yarnot.255@gmail.com>
2023-04-08 15:54:07 -0400

    Appease the all-powerful linter

--- surface/surface_main/launch/surface_operator_launch.py
+++ surface/surface_main/launch/surface_operator_launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
     gui_path: str = get_package_share_directory('gui')
     task_selector_path: str = get_package_share_directory('task_selector')
     transceiver_path: str = get_package_share_directory('transceiver')
-    
+
     # Launches Gui
     gui_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
