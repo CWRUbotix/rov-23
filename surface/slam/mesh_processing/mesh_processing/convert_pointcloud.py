bcf8b7dcfcef592ae21d263e3d544785cc4b9d75
NoahMollerstuen <noah@mollerstuen.com>
2023-04-08 15:57:30 -0400

    Use open3d to stitch coral pointcloud into a mesh

---
+++ surface/slam/mesh_processing/mesh_processing/convert_pointcloud.py
@@ -0,0 +1,58 @@
+import math
+import struct
+from sensor_msgs.msg import PointCloud2
+
+
+def read_points(cloud, field_names=None, skip_nans=False, uvs=[]):
+    """
+    Read points from a L{sensor_msgs.PointCloud2} message.
+    @param cloud: The point cloud to read from.
+    @type  cloud: L{sensor_msgs.PointCloud2}
+    @param field_names: The names of fields to read. If None, read all fields. [default: None]
+    @type  field_names: iterable
+    @param skip_nans: If True, then don't return any point with a NaN value.
+    @type  skip_nans: bool [default: False]
+    @param uvs: If specified, then only return the points at the given coordinates. [default: empty list]
+    @type  uvs: iterable
+    @return: Generator which yields a list of values for each point.
+    @rtype:  generator
+    """
+    assert isinstance(cloud, PointCloud2), 'cloud is not a sensor_msgs.msg.PointCloud2'
+    fmt = "<fffxxxxBBB"
+    width, height, point_step, row_step, data, isnan = cloud.width, cloud.height, cloud.point_step, cloud.row_step, cloud.data, math.isnan
+    unpack_from = struct.Struct(fmt).unpack_from
+
+    if skip_nans:
+        if uvs:
+            for u, v in uvs:
+                p = unpack_from(data, (row_step * v) + (point_step * u))
+                has_nan = False
+                for pv in p:
+                    if isnan(pv):
+                        has_nan = True
+                        break
+                if not has_nan:
+                    yield p
+        else:
+            for v in range(height):
+                offset = row_step * v
+                for u in range(width):
+                    p = unpack_from(data, offset)
+                    has_nan = False
+                    for pv in p:
+                        if isnan(pv):
+                            has_nan = True
+                            break
+                    if not has_nan:
+                        yield p
+                    offset += point_step
+    else:
+        if uvs:
+            for u, v in uvs:
+                yield unpack_from(data, (row_step * v) + (point_step * u))
+        else:
+            for v in range(height):
+                offset = row_step * v
+                for u in range(width):
+                    yield unpack_from(data, offset)
+                    offset += point_step
--- surface/slam/mesh_processing/setup.py
+++ surface/slam/mesh_processing/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'coral_processing_node = mesh_processing.coral_processing:main',
         ],
     },
 )
