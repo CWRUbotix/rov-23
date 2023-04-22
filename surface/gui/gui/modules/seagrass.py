503df049458c2fa67e673d8aeda499f28704d210
georgia-martinez <gcm49@case.edu>
2023-04-21 21:59:36 -0400

    Calculates the before and after difference

--- surface/gui/gui/modules/seagrass.py
+++ surface/gui/gui/modules/seagrass.py
@@ -1,7 +1,7 @@
 import sys
 import numpy as np
 from typing import List
-from PyQt5.QtWidgets import QWidget, QPushButton, QGridLayout, QApplication, QHBoxLayout
+from PyQt5.QtWidgets import QWidget, QPushButton, QGridLayout, QApplication, QHBoxLayout, QVBoxLayout, QLabel
 
 class SeagrassButton(QPushButton):
     def __init__(self, size: int):
@@ -28,6 +28,8 @@ class SeagrassButton(QPushButton):
             new_color = "white"
 
         self.color = new_color
+        self.recovered = not self.recovered
+
         self.setStyleSheet("background-color :" + new_color)
 
 class SeagrassGrid():
@@ -45,7 +47,7 @@ class SeagrassGrid():
                 self.all_buttons.append(seagrass_button)
 
     def get_num_recovered(self) -> int:
-        num_recovered: List[bool] = [button.covered for button in self.all_buttons]
+        num_recovered: List[bool] = [button.recovered for button in self.all_buttons]
 
         return np.count_nonzero(num_recovered)
 
@@ -57,15 +59,53 @@ class Seagrass(QWidget):
 
     def init_UI(self):
         self.resize(1200, 600)
-        self.root_layout = QHBoxLayout(self)
-        
-        actual = SeagrassGrid()
-        expected = SeagrassGrid()
 
-        self.root_layout.addLayout(actual.root_layout)
-        self.root_layout.addLayout(expected.root_layout)
+        self.root_layout = QHBoxLayout(self)
+
+        self.before_grid = SeagrassGrid()
+        self.after_grid = SeagrassGrid()
+
+        self.root_layout.addLayout(self.before_grid.root_layout)
+        self.root_layout.addLayout(self.after_grid.root_layout)
+
+        # Panel with result text and get difference button
+        self.result_layout = QVBoxLayout()
+
+        get_diff_button = QPushButton("Get Difference")
+        get_diff_button.clicked.connect(self.get_diff)
+
+        self.before_label = QLabel()
+        self.after_label = QLabel()
+        self.diff_label = QLabel()
+
+        self.result_layout.addWidget(self.before_label)
+        self.result_layout.addWidget(self.after_label)
+        self.result_layout.addWidget(self.diff_label)
+        self.result_layout.addWidget(get_diff_button)
+
+        self.root_layout.addLayout(self.result_layout)
 
         self.show()
+
+    def get_diff(self) -> None:
+        before_num = self.before_grid.get_num_recovered()
+        after_num = self.after_grid.get_num_recovered()
+
+        diff = abs(after_num - before_num)
+
+        result: str
+
+        if after_num > before_num:
+            result = f"{diff} went from white to green"
+        elif after_num < before_num:
+            result = f"{diff} went from green to white"
+        else:
+            result = "There was no change"
+
+        self.before_label.setText(f"Before: {before_num} green")
+        self.after_label.setText(f"After: {after_num} green")
+
+        self.diff_label.setText(result)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
