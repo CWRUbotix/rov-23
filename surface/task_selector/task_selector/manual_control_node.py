e742d205a4e52ec1bd3a2ef03f5a351447c97d09
Michael Carlstrom <rmc170@case.edu>
2023-04-21 19:00:40 -0700

    lint

--- surface/task_selector/task_selector/manual_control_node.py
+++ surface/task_selector/task_selector/manual_control_node.py
@@ -151,12 +151,12 @@ class ManualControlNode(Node):
                 new_manip_state: bool = not manip_button.is_active
                 manip_button.is_active = new_manip_state
 
-                log_msg: str = f"manip_id= {manip_button.claw}+ manip_active= {manip_button.is_active}"
+                log_msg: str = f"manip_id= {manip_button.claw}+ manip_active= {new_manip_state}"
                 self.get_logger().info(log_msg)
 
             manip_button.last_button_state = just_pressed
 
-            manip_msg: Manip = Manip(manip_id=manip_button.claw, 
+            manip_msg: Manip = Manip(manip_id=manip_button.claw,
                                      activated=manip_button.is_active)
             self.manip_publisher.publish(manip_msg)
 
