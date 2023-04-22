537acdbc4922ca779110f9e985ba8a0d7e78b594
georgia-martinez <gcm49@case.edu>
2023-04-21 20:58:32 -0400

    Moved grid layout code to SeagrassGrid class

--- surface/gui/gui/modules/seagrass.py
+++ surface/gui/gui/modules/seagrass.py
@@ -1,37 +1,51 @@
 import sys
+import numpy as np
+from typing import List
 from PyQt5.QtWidgets import QWidget, QPushButton, QGridLayout, QApplication, QHBoxLayout
 
-class SeaGrass(QWidget):
-
+class SeagrassGrid():
     def __init__(self):
-        super().__init__()
-        
-        self.resize(600, 600)
-        self.initUI()
+        self.root_layout = QGridLayout()
 
-
-    def initUI(self):
-        root_layout = QGridLayout(self)
-        
         N = 8
         BUTTON_SIZE = 50
 
         for row in range(N): 
            for col in range(N): 
                 
-                button = QPushButton("", self)
+                button = QPushButton()
                 button.setFixedSize(BUTTON_SIZE, BUTTON_SIZE)
+                button.setStyleSheet("background-color : green")
+
                 button.clicked.connect(self.on_click)
 
-                root_layout.addWidget(button, row, col)
-
-        self.show()
-
+                self.root_layout.addWidget(button, row, col)
+        
+        self.grid: List[List[bool]] = np.full((N, N), False, dtype=bool)
+ 
     def on_click(self):
         print("PyQt5 button click")
+
+class Seagrass(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.init_UI()
+
+    def init_UI(self):
+        self.resize(1200, 600)
+        self.root_layout = QHBoxLayout(self)
+        
+        actual = SeagrassGrid()
+        expected = SeagrassGrid()
+
+        self.root_layout.addLayout(actual.root_layout)
+        self.root_layout.addLayout(expected.root_layout)
+
+        self.show()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    sea_grass = SeaGrass()
+    sea_grass = Seagrass()
     sys.exit(app.exec_())
