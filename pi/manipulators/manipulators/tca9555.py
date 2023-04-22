c666b38dd37344210241e6ab6d8dceb9548e9fce
Michael Carlstrom <rmc170@case.edu>
2023-04-22 08:08:13 -0700

    rename

--- pi/manipulators/manipulators/tca9555.py
+++
@@ -1,2 +0,0 @@
-class TCA9555():
-    pass
--- pi/manipulators/setup.py
+++ pi/manipulators/setup.py
@@ -35,7 +35,7 @@ setup(
     entry_points={
         'console_scripts': [
             'manipulator = manipulators.manipulators:main',
-            'test = manipulators.manip_test:main'
+            'test = manipulators.manip_tester:main'
         ],
     },
 )
