49080505198c5e64b51fdc9baba58d7aafa718af
ROV Laptop <rov@gmail.com>
2023-04-29 16:12:17 -0400

    Use symlinks

--- pi/camera_streamer/launch/camera_launch.py
+++ pi/camera_streamer/launch/camera_launch.py
@@ -4,49 +4,31 @@ from launch import LaunchDescription
 
 def generate_launch_description():
 
+    # USB 3.0
     front_cam_node: Node = Node(
-        package="usb_cam",
-        executable="usb_cam_node_exe",
+        package="v4l2_camera",
+        executable="v4l2_camera_node",
         namespace="front_cam",
         parameters=[
-            {"video_device": "/dev/video0"},
-            {"image_width": 640},
-            {"image_height": 480},
-            {"pixel_format": "mjpeg"}
+            {"video_device":
+             "/dev/v4l/by-id/usb-3.0_USB_Camera_3.0_USB_Camera-video-index0"},
+            {"image_size": [640, 480]}
         ]
     )
 
-    # Launch the front camera node
-    # front_cam_node: Node = Node(
-    #     package="v4l2_camera",
-    #     executable="v4l2_camera_node",
-    #     namespace="front_cam",
-    #     parameters=[
-    #         {"video_device": "/dev/video0"}
-    #     ]
-    # )
-
-    # Launch the front camera node
-    # bottom_cam_node: Node = Node(
-    #     package="v4l2_camera",
-    #     executable="v4l2_camera_node",
-    #     namespace="bottom_cam",
-    #     parameters=[
-    #         {"video_device": "/dev/video2"}
-    #     ]
-    # )
-
-    # manip_cam_node: Node = Node(
-    #     package="v4l2_camera",
-    #     executable="v4l2_camera_node",
-    #     namespace="manip_cam",
-    #     parameters=[
-    #         {"video_device": "/dev/video6"}
-    #     ]
-    # )
+    # h264 (fisheye)
+    bottom_cam_node: Node = Node(
+        package="v4l2_camera",
+        executable="v4l2_camera_node",
+        namespace="bottom_cam",
+        parameters=[
+            {"video_device":
+             "/dev/v4l/by-id/usb-H264_USB_Camera_H264_USB_Camera_2020032801-video-index0"},
+            {"image_size": [640, 480]}
+        ]
+    )
 
     return LaunchDescription([
         front_cam_node,
-        # bottom_cam_node,
-        # manip_cam_node
+        bottom_cam_node
     ])
