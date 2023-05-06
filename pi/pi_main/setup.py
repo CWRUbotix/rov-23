464661de565bd93eccbe3bcfb5345bfdd981eff4
Michael Carlstrom <rmc@carlstrom.com>
2023-05-06 15:24:35 -0700

    test

--- pi/pi_main/setup.py
+++ pi/pi_main/setup.py
@@ -41,6 +41,8 @@ for dir in dirs:
 if ws is None:
     raise ValueError("No workspace found")
 
+print(os.listdir(ws))
+
 # Robot Upstart wants *.launch.py so this copies around that
 src = os.path.join('~', ws, 'src', 'pi', package_name, 'launch', 'pi_launch.py')
 src_home = os.path.expanduser(src)
