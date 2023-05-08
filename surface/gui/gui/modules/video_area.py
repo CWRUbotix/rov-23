4e921191620f533bb8ac4e0e0a8436f12ba64972
ROV Laptop <cwru-rov-laptop@protonmail.com>
2023-05-08 16:11:01 -0400

    Clean up

--- surface/gui/gui/modules/video_area.py
+++ surface/gui/gui/modules/video_area.py
@@ -7,19 +7,26 @@ from gui.event_nodes.subscriber import GUIEventSubscriber
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from cv2 import Mat
+import cv2
 
 
-# This file has a lot commented out. Commented code is mostly used for
-# switching the "big" video, which isn't used in our layout currently.
-# This code has been left for now in case we want to recover that functionality.
+PILOT_VIDEO_WIDTH: int = 900
+PILOT_VIDEO_HEIGHT: int = 675
+
+
 class VideoWidget(QLabel):
     """A single video stream widget."""
 
     update_big_video_signal = pyqtSignal(QWidget)
     handle_frame_signal = pyqtSignal(Image)
 
-    def __init__(self, index: int, topic: str):
+    def __init__(self, index: int, topic: str, widget_width: int = 640,
+                 widget_height: int = 480, swap_rb_channels: bool = False):
         super().__init__()
+
+        self.widget_width: int = widget_width
+        self.widget_height: int = widget_height
+        self.swap_rb_channels: bool = swap_rb_channels
 
         self.index: int = index
 
@@ -35,29 +42,12 @@ class VideoWidget(QLabel):
         self.camera_subscriber: GUIEventSubscriber = GUIEventSubscriber(
             Image, topic, self.handle_frame_signal)
 
-    # def mousePressEvent(self, ev: QMouseEvent):
-    #     """Swap this video with the big video on click."""
-    #     if self.index != 0:
-    #         self.update_big_video_signal.emit(self)
-
     @pyqtSlot(Image)
     def handle_frame(self, frame: Image):
         cv_image: Mat = self.cv_bridge.imgmsg_to_cv2(
             frame, desired_encoding='passthrough')
 
-        # TODO: dynamic image scaling based on Qt element size
-        # qt_image: QImage = self.convert_cv_qt(
-        #     cv_image,
-        #     self.frameGeometry().width(),
-        #     self.frameGeometry().height()
-        # )
-
-        qt_image: QImage = self.convert_cv_qt(cv_image, 900, 675)
-
-        # self.setPixmap(qt_image.scaled(
-        #     self.frameGeometry().width(),
-        #     self.frameGeometry().height(),
-        #     Qt.KeepAspectRatio))
+        qt_image: QImage = self.convert_cv_qt(cv_image, self.widget_width, self.widget_height)
 
         self.setPixmap(QPixmap.fromImage(qt_image))
 
@@ -65,8 +55,10 @@ class VideoWidget(QLabel):
         """Convert from an opencv image to QPixmap."""
         # Color image
         if len(cv_img.shape) == 3:
-            # Rectify weird colors
-            # cv_img = cv2.cvtColor(cv_img, cv2.COLOR_RGB2BGR)
+            # Swap red & blue channels if necessary
+            if self.swap_rb_channels:
+                cv_img = cv2.cvtColor(cv_img, cv2.COLOR_RGB2BGR)
+
             h, w, ch = cv_img.shape
             bytes_per_line: int = ch * w
 
@@ -91,7 +83,6 @@ class VideoWidget(QLabel):
 class VideoArea(QWidget):
     """Container widget handling all video streams."""
 
-    # First entry here will start as the big video
     CAMERA_TOPICS = ['/front_cam/image_raw', '/bottom_cam/image_raw']
     CAMERA_COORDS = [(0, 0), (0, 1)]
 
@@ -105,47 +96,11 @@ class VideoArea(QWidget):
         self.grid_layout.setColumnStretch(0, 1)
         self.grid_layout.setColumnStretch(1, 1)
 
-        # MAGIC VALUE WARNING: i=0 represents the big video
         self.video_widgets: list[VideoWidget] = []
 
         for i, topic in enumerate(self.CAMERA_TOPICS):
-            video: VideoWidget = VideoWidget(i, topic)
+            video: VideoWidget = VideoWidget(i, topic, PILOT_VIDEO_WIDTH, PILOT_VIDEO_HEIGHT)
             self.video_widgets.append(video)
 
             self.grid_layout.addWidget(video, self.CAMERA_COORDS[i][0],
                                        self.CAMERA_COORDS[i][1], 1, 3)
-
-            # video.update_big_video_signal.connect(self.set_as_big_video)
-
-            # if i == 0:
-            #     self.grid_layout.addWidget(video, 0, 0, 1, 3)
-            # else:
-            #     self.grid_layout.addWidget(video, 1, i - 1, 1, 1)
-
-    # @pyqtSlot(QWidget)
-    # def set_as_big_video(self, target_widget: VideoWidget):
-    #     """Swap target VideoWidget with big VideoWidget."""
-    #     big_widget: QWidget = self.grid_layout.itemAtPosition(
-    #         0, 0).widget()
-
-    #     if isinstance(big_widget, VideoWidget):
-    #         self.grid_layout.removeWidget(target_widget)
-    #         self.grid_layout.removeWidget(big_widget)
-
-    #         big_widget.index = target_widget.index
-    #         target_widget.index = 0  # 0 still represents the big video
-
-    #         small_frame_geometry = target_widget.frameGeometry()
-
-    #         self.grid_layout.addWidget(target_widget, 0, 0, 1, 3)
-    #         self.grid_layout.addWidget(big_widget, 1, big_widget.index - 1, 1, 1)
-
-    #         target_widget.setPixmap(QPixmap.fromImage(target_widget.qt_image.scaled(
-    #             big_widget.frameGeometry().width(),
-    #             big_widget.frameGeometry().height())))
-
-    #         big_widget.setPixmap(QPixmap.fromImage(big_widget.qt_image.scaled(
-    #             small_frame_geometry.width(),
-    #             small_frame_geometry.height())))
-    #     else:
-    #         RcutilsLogger("video_area.py").fatal("big_widget is not a VideoWidget")
