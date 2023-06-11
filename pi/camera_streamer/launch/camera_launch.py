01acff65e43c4930d55119815c06c725d421d514
Georgia <gcm49@case.edu>
2023-06-11 17:44:07 +0000

    updated camid

--- pi/camera_streamer/launch/camera_launch.py
+++ pi/camera_streamer/launch/camera_launch.py
@@ -16,7 +16,7 @@ def generate_launch_description():
         namespace="front_cam",
         parameters=[
             {"video_device":
-             "/dev/v4l/by-id/usb-3.0_USB_Camera_3.0_USB_Camera_2020042501-video-index0"},
+             "/dev/v4l/by-id/usb-3.0_USB_Camera_3.0_USB_Camera-video-index0"},
             {"image_size": [640, 480]},
             {"time_per_frame": [1, 30]}
         ],
@@ -31,7 +31,7 @@ def generate_launch_description():
         namespace="bottom_cam",
         parameters=[
             {"video_device":
-             "/dev/v4l/by-id/usb-3.0_USB_Camera_3.0_USB_Camera-video-index0"},
+             "/dev/v4l/by-id/usb-3.0_USB_Camera_3.0_USB_Camera_2020042501-video-index0"},
             {"image_size": [640, 480]},
             {"time_per_frame": [1, 30]}
         ],
