5126fadf6086a4fe23cd9cf356cb7c6452605fd3
Georgia <gcm49@case.edu>
2023-06-21 15:20:29 +0000

    Remap depth cam topics

--- pi/realsense/launch/realsense_launch.py
+++ pi/realsense/launch/realsense_launch.py
@@ -1,8 +1,9 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, GroupAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import SetRemap
 
 
 def generate_launch_description():
@@ -18,6 +19,15 @@ def generate_launch_description():
         ]),
     )
 
+    realsense_action = GroupAction(
+        actions=[
+            SetRemap(src='/pi/camera/color/image_raw', dst='/depth_cam/image_raw'),
+            SetRemap(src='/pi/camera/color/camera_info', dst='/depth_cam/camera_info'),
+            SetRemap(src='/pi/camera/aligned_depth_to_color/image_raw', dst='/depth_cam/depth_to_color'),
+            realsense_launch
+        ]
+    )
+
     return LaunchDescription([
-        realsense_launch,
+        realsense_action
     ])
