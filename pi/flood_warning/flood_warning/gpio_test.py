b90a551de65e561f63732869cf07fdc0ad9be767
Georgia <gcm49@case.edu>
2023-06-21 14:13:58 +0000

    GPIO package testing without ROS

---
+++ pi/flood_warning/flood_warning/gpio_test.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+
+import lg
+
+# GPIO PIN #
+PIN = 26
+
+def main():
+    # GPIO.BCM because we use a Compute Module
+    PIN = 26
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(PIN, GPIO.OUT)
+
+    while True:
+        print(GPIO.input(PIN))
+
+        if GPIO.input(PIN) == GPIO.HIGH:
+            print("Flooding detected")
+            print(PIN)
+            raise
+        PIN += 1
+
+        if PIN > 50:
+            PIN = 0
+
+if __name__ == '__main__':
+    main()
