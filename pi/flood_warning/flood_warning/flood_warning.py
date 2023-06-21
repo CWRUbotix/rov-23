59260fc25bbc87017cc175a828f485b77f5c666b
Tyler Zupfer <tzupfer@gmail.com>
2023-06-21 10:28:19 -0400

    In theory this will just work

--- pi/flood_warning/flood_warning/flood_warning.py
+++ pi/flood_warning/flood_warning/flood_warning.py
@@ -1,10 +1,14 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Bool
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
+import lgpio as GPIO
 
 # GPIO PIN #
 PIN = 26
+
+h = GPIO.gpiochip_open(0)
+GPIO.gpio_claim_input(h, PIN)
 
 
 class FloodWarning(Node):
@@ -20,17 +24,17 @@ class FloodWarning(Node):
         )
 
         # GPIO.BCM because we use a Compute Module
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(PIN, GPIO.OUT)
+        # GPIO.setmode(GPIO.BCM)
+        # GPIO.setup(PIN, GPIO.OUT)
         self.check_input()
 
     # https://www.ics.com/blog/control-raspberry-pi-gpio-pins-python
     # Investigate adding callback for when in is pulled high maybe?
     def check_input(self):
         while True:
-            self.pub.publish(Bool(GPIO.input(PIN)))
+            self.pub.publish(Bool(GPIO.gpio_read(h, PIN)))
 
-            if GPIO.input(PIN) == GPIO.HIGH:
+            if GPIO.gpio_read(h, PIN):
                 self.get_logger().error("FLOODING DETECTED!")
 
 
